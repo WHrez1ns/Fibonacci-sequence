2667a253b085f079fb51aee0028e5e225aacc60b
Renan <renan.dias0871@gmail.com>
2023-04-01 14:17:05 -0300

    defining comments

--- fibonacci.py
+++ fibonacci.py
@@ -1,36 +1,50 @@
 # Caio Godoy - RM 98002
 # Júlia Barboza - RM 98558
-# Nicholas Calegari - RM 93912 
+# Nicholas Calegari - RM 93912
 # Renan Dias da Costa Silva - RM 99258
 # Victor Tosto - RM 99599
 
+# Título
 print('-'*28)
 print('|  Sequência de Fibonacci  |')
 print('-'*28)
 
+# Definindo a função que exibe os termos da sequência
+def fibonacci_sequence(termo_1, termo_2, contador):
+    # Exibindo os dois primeiros termos | parâmetro end para não pular linha
+    print(f'{termo_1} -> {termo_2}', end='')
 
-def fibonacci_sequence(t1, t2, n):
-    print(f'{t1} -> {t2}', end='')
+    # Para cada número na sequência de 2 até o número N
+    for num in range(2, contador):
+        # Definindo o terceiro termo
+        term_3 = term_1 + term_2
 
-    for i in range(2, n):
-        t3 = t1 + t2
-        print(f' -> {t3}', end='')
+        # Exibindo o terceiro termo
+        print(f' -> {term_3}', end='')
 
-        t1 = t2
-        t2 = t3
+        # Passando a sequência para frente
+        term_1 = term_2
+        term_2 = term_3
 
+    # Definindo o fim da sequência
     print(' -> end sequence')
     print(' ')
 
 
+# Definindo o loop infinito
 while True:
+    # Pulando linha
     print(' ')
+
+    # Lendo o número inteiro N (contador)
     print('~'*52)
     n = int(input("Quantos termos você quer mostrar? "))
 
+    # Chamando a função e passando os dois primeiros termos e N como contador
     print(' ')
     fibonacci_sequence(0, 1, n)
 
+    # Definindo a resposta
     resp = input('Deseja continuar? (S/N): ').upper()
     if resp == 'N':
         break
